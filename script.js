fa74bdcf1e593d62d7e2336dde3de7cedfa80ebc
Nagy-Zsugya Martin <98815314+Etereke@users.noreply.github.com>
2024-02-20 10:32:31 +0100

    Add basic book display

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const tBody = document.querySelector('tbody');
+
 const myLibrary = [];
+
+addBookToLibrary('The Hobbit', 'J. R. R. Tolkien', 432);
+addBookToLibrary('The Hobbit', 'J. R. R. Tolkien', 432);
+addBookToLibrary('The Hobbit', 'J. R. R. Tolkien', 432);
+addBookToLibrary('The Hobbit', 'J. R. R. Tolkien', 432);
+addBookToLibrary('The Hobbit', 'J. R. R. Tolkien', 432);
+
+displayBooks();
 
 function Book(title, author, pages) {
     this.title = title;
@@ -15,3 +25,23 @@ function addBookToLibrary(title, author, pages) {
     myLibrary.push(new Book(title, author, pages));
 }
 
+function displayBooks() {
+    for (book of myLibrary) {
+        const tr = document.createElement('tr');
+        for (key in book) {
+            const td = document.createElement('td');
+            if (key === 'read') {
+                const deleteBtn = document.createElement('button');
+                deleteBtn.textContent = "Delete";
+                const readBtn = document.createElement('button');
+                readBtn.textContent = "Toggle read";
+                td.appendChild(readBtn);
+                td.appendChild(deleteBtn);
+            } else {
+                td.textContent = book[key];
+            }
+            tr.appendChild(td);
+        }
+        tBody.appendChild(tr);
+    }
+}
